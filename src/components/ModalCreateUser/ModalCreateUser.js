161308441c72a3d622c3e3ce4f915da317fa6844
dangvannho <dangvannho7@gmail.com>
2024-08-25 20:02:57 +0700

    API Thêm Mới Người Dùng (CREATE)

--- src/components/ModalCreateUser/ModalCreateUser.js
+++ src/components/ModalCreateUser/ModalCreateUser.js
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FaPlusCircle } from "react-icons/fa";
 import "./ModalCreateUser.scss";
 
-function ModalCreateUser() {
-  const [show, setShow] = useState(false);
+function ModalCreateUser({ show, setShow }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [role, setRole] = useState("USER");
   const [image, setImage] = useState("");
-  const [previewImage, setPreviewImage] = useState();
+  const [previewImage, setPreviewImage] = useState("");
+
+  // Xử lý rò rỉ bộ nhớ
+  useEffect(() => {
+    return () => {
+      URL.revokeObjectURL(previewImage);
+    };
+  }, [previewImage]);
 
   const handleUploadImage = (e) => {
     if (e.target.files[0]) {
@@ -20,97 +27,119 @@ function ModalCreateUser() {
     }
   };
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const reset = () => {
+    setEmail("");
+    setPassword("");
+    setUsername("");
+    setRole("USER");
+    setImage("");
+    setPreviewImage("");
+  };
 
+  const handleClose = () => {
+    setShow(false);
+    reset();
+  };
+
+  const handleSubmitCreareUSer = async () => {
+    // call api
+    const data = new FormData();
+    data.append("email", email);
+    data.append("password", password);
+    data.append("username", username);
+    data.append("role", role);
+    data.append("userImage", image);
+
+    const res = await axios.post(
+      "http://localhost:8081/api/v1/participant",
+      data
+    );
+    console.log(res);
+    setShow(false);
+    reset();
+  };
   return (
-    <>
-      <Button variant="secondary" onClick={handleShow}>
-        Add new user
-      </Button>
+    <Modal show={show} onHide={handleClose} size="xl" backdrop="static">
+      <Modal.Header closeButton>
+        <Modal.Title>Add new user</Modal.Title>
+      </Modal.Header>
 
-      <Modal show={show} onHide={handleClose} size="xl" backdrop="static">
-        <Modal.Header closeButton>
-          <Modal.Title>Add new user</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <form>
-            <div className="form-row ">
-              <div className="form-group col-md-6">
-                <label>Email</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="form-group col-md-6">
-                <label>Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-            </div>
-
-            <div className="form-row">
-              <div className="form-group col-md-6">
-                <label>Username</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </div>
-              <div className="form-group col-md-4">
-                <label>Role</label>
-                <select
-                  className="form-control"
-                  onChange={(e) => setRole(e.target.value)}
-                >
-                  <option value="USER">USER</option>
-                  <option value="ADMIN">ADMIN</option>
-                </select>
-              </div>
-            </div>
-
-            <div className="form-group col-md-12 upload">
-              <label htmlFor="label-upload" className="label-upload">
-                <FaPlusCircle />
-                Upload File Image
-              </label>
+      <Modal.Body>
+        <form>
+          <div className="form-row ">
+            <div className="form-group col-md-6">
+              <label>Email</label>
               <input
-                type="file"
-                hidden
-                id="label-upload"
-                onChange={(e) => handleUploadImage(e)}
+                type="email"
+                className="form-control"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
-
-            <div className="form-group col-md-12 img-preview">
-              {previewImage ? (
-                <img src={previewImage} alt="" />
-              ) : (
-                <span>Preview Image</span>
-              )}
+            <div className="form-group col-md-6">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-          </form>
-        </Modal.Body>
+          </div>
 
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-group col-md-4">
+              <label>Role</label>
+              <select
+                className="form-control"
+                onChange={(e) => setRole(e.target.value)}
+              >
+                <option value="USER">USER</option>
+                <option value="ADMIN">ADMIN</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="form-group col-md-12 upload">
+            <label htmlFor="label-upload" className="label-upload">
+              <FaPlusCircle />
+              Upload File Image
+            </label>
+            <input
+              type="file"
+              hidden
+              id="label-upload"
+              onChange={(e) => handleUploadImage(e)}
+            />
+          </div>
+
+          <div className="form-group col-md-12 img-preview">
+            {previewImage ? (
+              <img src={previewImage} alt="" />
+            ) : (
+              <span>Preview Image</span>
+            )}
+          </div>
+        </form>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleSubmitCreareUSer}>
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
--- src/pages/Admin/ManageUser/ManageUser.js
+++ src/pages/Admin/ManageUser/ManageUser.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import { FaPlusCircle } from "react-icons/fa";
 import ModalCreateUser from "~/components/ModalCreateUser";
 import "./ManageUser.scss";
 
 function ManageUser() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="manage-user_container">
       <h2 className="manage-user_title">Manage User</h2>
       <div className="manage-user_content">
-        <ModalCreateUser />
+        <button
+          className="btn btn-secondary"
+          onClick={() => setShowModal(true)}
+        >
+          <FaPlusCircle />
+          Add new user
+        </button>
         <div className="manage-user_table">table user</div>
+        <ModalCreateUser show={showModal} setShow={setShowModal} />
       </div>
     </div>
   );
